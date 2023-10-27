8de06fc08dd27cea323bbfb560f3352d56ffd3da
sudev.s.kumar <sudev.kumar@qburst.com>
2023-10-27 16:35:20 +0530

    slight modf

--- src/components/Header/HeaderInput.jsx
+++ src/components/Header/HeaderInput.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 function HeaderInput({setProduct , title}){
+        let inputRef = useRef()
+        inputRef.current && inputRef.current.focus()
         var value = JSON.parse(localStorage.getItem("search")) || ""
        const navigate = useNavigate()
         function handleChange(e){
                 console.log("chnaged");
               setProduct(e.target.value)
               localStorage.setItem("search",JSON.stringify(e.target.value))
-             if(title !== "cart") navigate("/")
+             if(title !== "cart"){
+                navigate("/")
+                
+                
+             }
 
         }
     return(
         <div className="flex rounded-3xl bg-white w-[40%] items-center">
                 <img className="p-2 pr-0" src="/search.png" alt="" />
-                <input onChange={handleChange} className="pl-2 text-lg outline-none bg-transparent w-full" value={value} ></input>
+                <input ref={inputRef} id="search" onChange={handleChange} className="pl-2 text-lg outline-none bg-transparent w-full" value={value} ></input>
         </div>
 )}
 
